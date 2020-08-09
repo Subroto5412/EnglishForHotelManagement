000495bcd207c23ac71b30b84af0a27a3860f66e
Subroto Mohonto <subroto.mohonto@gmail.com>
2020-08-09 17:31:16 +0600

    Slider Screen

--- app/src/main/java/com/subroto/english/hotelmanagement/MainActivity.kt
+++ app/src/main/java/com/subroto/english/hotelmanagement/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.subroto.english.hotelmanagement
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.subroto.english.hotelmanagement.R
+import kotlinx.android.synthetic.main.activity_main.*
+import me.relex.circleindicator.CircleIndicator3
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var titlesList = mutableListOf<String>()
+    private var subTitlesList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
+
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar()?.hide(); //hide the title bar
         setContentView(R.layout.activity_main)
+        listDataManager()
+
+        view_pager2.adapter =
+            ViewPagerAdapter(
+                titlesList,
+                subTitlesList,
+                imagesList
+            )
+        view_pager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val indicator: CircleIndicator3 = findViewById<CircleIndicator3>(R.id.indicator)
+        indicator.setViewPager(view_pager2)
+    }
+
+    private fun addToListModel(title: String, subtTitle: String, image: Int) {
+        titlesList.add(title)
+        subTitlesList.add(subtTitle)
+        imagesList.add(image)
+    }
+
+    private fun listDataManager() {
+        for (i: Int in 1..3) {
+            if (i == 1) {
+                addToListModel(title = getString(R.string.slider_title1),subtTitle = getString(
+                    R.string.slider_subtitle1
+                ), image = R.drawable.slider_pic1
+                )
+            } else if (i == 2) {
+                addToListModel(title = getString(R.string.slider_title2),subtTitle = getString(
+                    R.string.slider_subtitle2
+                ), image = R.drawable.slider_pic2
+                )
+            } else if (i == 3) {
+                addToListModel(title = getString(R.string.slider_title3),subtTitle = getString(
+                    R.string.slider_subtitle3
+                ), image = R.drawable.slider_pic3
+                )
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/subroto/english/hotelmanagement/ViewPagerAdapter.kt
@@ -0,0 +1,51 @@
+package com.subroto.english.hotelmanagement
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.subroto.english.hotelmanagement.R
+
+class ViewPagerAdapter(
+    private var title: List<String>,
+    private var subTitle: List<String>,
+    private var images: List<Int>
+) :
+    RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>() {
+
+    inner class Pager2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemTitle: TextView = itemView.findViewById(R.id.title)
+        val itemSubTitle: TextView = itemView.findViewById(R.id.sub_title)
+        val images: ImageView = itemView.findViewById(R.id.slider_img)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): Pager2ViewHolder {
+
+        return Pager2ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.index_page, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return title.size
+    }
+
+    override fun onBindViewHolder(holder: Pager2ViewHolder, position: Int) {
+        holder.itemTitle.text = title[position]
+        holder.itemSubTitle.text = subTitle[position]
+        holder.images.setImageResource(images[position])
+        if (position == 0) {
+            holder.itemTitle.setTextColor(Color.parseColor("#0D81FF"));
+        } else if (position == 1) {
+            holder.itemTitle.setTextColor(Color.parseColor("#30D64C"));
+        } else if (position == 2) {
+            holder.itemTitle.setTextColor(Color.parseColor("#FF7A00"));
+        }
+    }
+}
